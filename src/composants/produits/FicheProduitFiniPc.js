3695d1dcf2546dc382d16805903169eace7f51df
ROUX YVAN <yvan.roux@inrae.fr>
2024-04-11 16:01:54 +0200

    fin partie benefits (ﾉ≧∀≦)ﾉ ‥…━━━★

--- src/composants/produits/FicheProduitFiniPc.js
+++ src/composants/produits/FicheProduitFiniPc.js
@@ -103,6 +103,13 @@ export default function FicheProduitFiniPc({imgName, completeName, shortDesc, in
                         </a>
                     </div>
                 </div>
+                <div className="niosomal-techno">
+                    <h3 className='text title'>NIOSOMAL TECHNOLOGY®</h3>
+                    <p className="text pargraph">At Akore Formulations, we're revolutionizing the world of cosmetics through innovation and scientific expertise. We use niosome technology to push back the boundaries of cosmetic efficacy.</p>
+                    <p className="text pargraph">This non-ionic encapsulation system is designed to enhance the delivery of active ingredients to the skin. Thanks to their unique structure, they effectively encapsulate ingredients, enabling controlled release and better penetration into the skin.</p>
+                    <p className="text pargraph">The result is a significant increase in the efficacy of cosmetic products, while ensuring optimum protection of sensitive active ingredients. <br/>
+                        In collaboration with dermatology experts, we offer highly concentrated, premium-quality cosmetics.</p>
+                </div>
             </div>
 
             {/*--------------- block ingredients -------------------------*/}
