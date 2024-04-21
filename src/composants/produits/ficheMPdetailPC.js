2507dc608164a7c526c6fce0cac45a5df6a630dc
Yvan Roux <yvis99@gmail.com>
2024-04-21 21:01:22 +0200

    partie benefits finie

--- src/composants/produits/ficheMPdetailPC.js
+++ src/composants/produits/ficheMPdetailPC.js
@@ -4,17 +4,25 @@ import '../../utils/css/produits/fiche produits finis/FicheProduitFiniPc.css'
 import ProductTripleList from "./ProductTripleList";
 import OtherProducts from "./OtherProducts";
 import FicheProduitGeneralDescription from "./FicheProduitGeneralDescription";
-import FPFCheckBoxBenefits from "./fiche produits finis/FPFCheckBoxBenefits";
-import NiosomalTechnoParagraph from "./NiosomalTechnoParagraph";
+import MPCheckBoxBenefits from "./MPCheckBoxBenefits";
 
-export default function FicheMPdetailPC({productKey, imgName, name, shortDesc, paragraphsDescList, checkboxDescList, benefitsList, ingredientParagraph, howToUseParagraphsList, precautionsParagraphsList}) {
+export default function FicheMPdetailPC({
+                                            productKey,
+                                            imgName,
+                                            name,
+                                            shortDesc,
+                                            paragraphsDescList,
+                                            checkboxDescList,
+                                            benefitsList,
+                                            ingredientParagraph,
+                                            howToUseParagraphsList,
+                                            precautionsParagraphsList
+                                        }) {
     const imageProduct = require(`../../images/AKORE WEBSITE/PRODUITS/${imgName}`)
     const imageLiposome = require('../../images/AKORE WEBSITE/IMAGES/liposome.png')
 
     const plusPicto = require('../../images/AKORE WEBSITE/PICTOS/plus.png')
-    const francePicto = require('../../images/AKORE WEBSITE/PICTOS/france.png')
-    const facePicto = require('../../images/AKORE WEBSITE/PICTOS/face.png')
-    const liposomePicto = require('../../images/AKORE WEBSITE/PICTOS/liposome.png')
+    const imageGoutte = require('../../images/AKORE WEBSITE/IMAGES/fiche-MP-goutte.png')
 
     return (
         <div className='container'>
@@ -22,7 +30,7 @@ export default function FicheMPdetailPC({productKey, imgName, name, shortDesc, p
             <div id='description-block'>
                 <img className='MP-image' src={imageProduct} alt={imgName}/>
                 <FicheProduitGeneralDescription
-                    completeName={name+" NISOSOME SERUM"}
+                    completeName={name + " NIOSOME SERUM"}
                     shortDesc={shortDesc}
                     paragraphsDescList={paragraphsDescList}
                     checkboxDescList={checkboxDescList}
@@ -30,35 +38,21 @@ export default function FicheMPdetailPC({productKey, imgName, name, shortDesc, p
             </div>
 
             {/*--------------- block benefits -------------------------*/}
-            <div className='benefits-block'>
-                <div className='benefits-title underligned'>
-                    <div className='logo-and-image'>
-                        <img src={plusPicto} alt={"logo plus"}/>
-                        <h1 className='text title'>BENEFITS</h1>
-                    </div>
-                    <div className='benefits-logos-title'>
-                        <div className='logo-and-image small-text france-picto'>
-                            <img src={francePicto} alt={"france"}/>
-                            <p className='text title'>FRENCH <br/> LABORATORY</p>
-                        </div>
-                        <div className='logo-and-image small-text'>
-                            <img src={facePicto} alt={"face"}/>
-                            <p className='text title'>FACE <br/> SERUM</p>
-                        </div>
-                        <div className='logo-and-image small-text'>
-                            <img src={liposomePicto} alt={"logo liposome"}/>
-                            <p className='text title'>100% <br/> NISOMALES ACTIVES</p>
+            <div className='benefits-block-MP'>
+                <img src={imageGoutte}/>
+                <div className='benefits-MP'>
+                    <div className='benefits-title-MP'>
+                        <div className='logo-and-image'>
+                            <img src={plusPicto} alt={"logo plus"}/>
+                            <h2 className='text title'>BENEFITS</h2>
                         </div>
                     </div>
+                    <MPCheckBoxBenefits/>
+                    <a href="#ingredients" className={"discover-compo-MP"}>
+                        <p className={"underligned text"}> DISCOVER </p>
+                        <p className={"text"}>the complete composition</p>
+                    </a>
                 </div>
-                <div className='benefits-description'>
-                    <div className="benefits-left-block">
-                        <FPFCheckBoxBenefits/>
-                        <img src={imageLiposome} alt="liposome"/>
-
-                    </div>
-                </div>
-                <NiosomalTechnoParagraph/>
             </div>
 
             {/*--------------- block ingredients -------------------------*/}
